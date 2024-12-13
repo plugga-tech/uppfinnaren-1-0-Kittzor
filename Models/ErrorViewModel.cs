7b51a07de30b8ebac32a201719ad4b813f3289f1
Christoffer Kjellgren <kjellgrenchristoffer@gmail.com>
2024-12-13 12:29:24 +0100

    Added gitignore

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Uppfinnaren.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
