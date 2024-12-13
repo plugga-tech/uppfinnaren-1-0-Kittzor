8970642c6f8bb1d73434a4f98385881090ebf9f7
Christoffer Kjellgren <kjellgrenchristoffer@gmail.com>
2024-12-13 14:28:00 +0100

    added classes, views and changed controller

---
+++ Models/Alster.cs
@@ -0,0 +1,11 @@
+﻿namespace Uppfinnaren.Models
+{
+    public class Alster
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+
+    }
+}
