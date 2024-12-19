f57c4675133b1abff1b41cfa6a0ceec4381f7aa0
Christoffer Kjellgren <kjellgrenchristoffer@gmail.com>
2024-12-19 15:54:24 +0100

    added connections to database and created mock data

--- Models/Alster.cs
+++ Models/Alster.cs
@@ -3,6 +3,7 @@
     public class Alster
     {
         public int Id { get; set; }
+        public string Sort { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string ImageUrl { get; set; }
